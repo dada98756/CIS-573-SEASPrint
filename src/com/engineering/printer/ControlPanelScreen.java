c3998582170894c504a66f1c5082ef8102f4165a
Travis Shao <Travis Shao@TS-TP>
2012-11-02 17:07:37 -0400

    Merger Conflict & Update A little bit

--- src/com/engineering/printer/ControlPanelScreen.java
+++ src/com/engineering/printer/ControlPanelScreen.java
@@ -1,17 +1,33 @@
 package com.engineering.printer;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  *
  * @author Siyong Liang
  */
 public class ControlPanelScreen extends Activity{
+	private Button browserButton;
+	//private Button cloudButton;
+	//private Button printButton;
+	//private Button historytButton;
+	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.controlpanel);    
+        setContentView(R.layout.controlpanel);  
+        
+        browserButton = (Button) findViewById(R.id.file_browser_button);
+        browserButton.setEnabled(true);
     }
+    
+    public void onBrowserBtnClick(View v) {
+    	Intent myIntent = new Intent(v.getContext(), FilePicker.class);
+        startActivityForResult(myIntent, 0);
+	}
     
      public void onResume() {
             super.onResume();
